2e4f95d62d1f297d8adb5bc6876172bdd8256acb
avanegasp <arco527@gmail.com>
2017-08-30 09:04:16 -0500

    Works something

---
+++ app/app.js
@@ -0,0 +1,4 @@
+$('tr#uno').click(function(){
+  $('tr#dos').hide()
+  $('tr#dos').show('<td><a href ="<iframe width="640" height="360" src="https://www.youtube.com/embed/M7wo0BeN7Fw" frameborder="0" allowfullscreen></iframe>"</td>')
+});
