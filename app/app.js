96c40e1a3330ef674052edecb625956f2e843570
avanegasp <arco527@gmail.com>
2017-09-11 19:05:35 -0500

    Done

--- app/app.js
+++ app/app.js
@@ -1,10 +1,51 @@
 
-$('td').click(function(){
-  $('#list').hide()
-  $('.music').show()
-})
+  $('td').click(function(){
+    $('#list').hide()
+    $('.music').show()
+  })
 
-$('button').click(function(){
-  $('#list').show()
-  $('.music').hide()
-})
+  if($('.uno').click(function(){
+    $('#uno').show()
+    $('#dos').hide()
+    $('#tres').hide()
+    $('#cuatro').hide()
+    $('#cinco').hide()
+  }))
+
+  if($('.dos').click(function(){
+    $('#dos').show()
+    $('#uno').hide()
+    $('#tres').hide()
+    $('#cuatro').hide()
+    $('#cinco').hide()
+  }))
+
+  if($('.tres').click(function(){
+    $('#tres').show()
+    $('#uno').hide()
+    $('#dos').hide()
+    $('#cuatro').hide()
+    $('#cinco').hide()
+  }))
+
+  if($('.cuatro').click(function(){
+    $('#cuatro').show()
+    $('#uno').hide()
+    $('#dos').hide()
+    $('#tres').hide()
+    $('#cinco').hide()
+  }))
+
+  if($('.cinco').click(function(){
+    $('#cinco').show()
+    $('#uno').hide()
+    $('#dos').hide()
+    $('#tres').hide()
+    $('#cuatro').hide()
+  }))
+
+
+  $('button').click(function(){
+    $('#list').show()
+    $('.music').hide()
+  })
