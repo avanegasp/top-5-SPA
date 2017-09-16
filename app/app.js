f27d1a5ab24edb3fd7b81fd4cbed966bae39744b
avanegasp <arco527@gmail.com>
2017-09-16 18:46:05 -0500

    Play list with video, done(add and removeClass)

--- app/app.js
+++ app/app.js
@@ -1,35 +1,14 @@
 
-  $('td').click(function(){
-    $('#list').hide()
-    $('.music').show()
-  })
+  $('tr').click(function(evento){
+  $('#list').hide()
+  var tr = $(evento.currentTarget)
+  var dataAtrr = tr.attr('data-artist')
 
-  $('.uno').click(function(){
-    $('.music').hide()
-    $('#uno').show()
-  })
-
-  $('.dos').click(function(){
-    $('.music').hide()
-    $('#dos').show()
-  })
-
-  $('.tres').click(function(){
-    $('.music').hide()
-    $('#tres').show()
-  })
-
-  $('.cuatro').click(function(){
-    $('.music').hide()
-    $('#cuatro').show()
-  })
-
-  $('.cinco').click(function(){
-    $('.music').hide()
-    $('#cinco').show()
-  })
+  $('section').removeClass('active')
+  $(dataAtrr).addClass('active')
+})
 
   $('button').click(function(){
-    $('#list').show()
-    $('.music').hide()
-  })
+  $('#list').show()
+  $('section').removeClass('active')
+})
